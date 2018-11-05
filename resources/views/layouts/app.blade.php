aad005e39018bba47f68fc2e5509a91cacd9ea8d
takashi <takashioicsn@gmail.com>
2018-11-06 04:00:54 +0900

    modified layouts/app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
+  <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -18,63 +18,67 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-</head>
-<body>
+  </head>
+  <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    Fish i
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+      <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+        <div class="container">
+          <a class="navbar-brand" href="{{ url('/') }}">
+            Fish i
+          </a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+            <span class="navbar-toggler-icon"></span>
+          </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <!-- Left Side Of Navbar -->
+            <ul class="navbar-nav mr-auto"></ul>
 
-                    </ul>
+            <!-- Right Side Of Navbar -->
+            <ul class="navbar-nav ml-auto">
+              <!-- Authentication Links -->
+              @guest
+                <li class="nav-item">
+                  <a class="nav-link" href="{{ url("/list") }}">ちくわ一覧</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="{{ route('login') }}">ログイン</a>
+                </li>
+                <li class="nav-item">
+                  @if (Route::has('register'))
+                    <a class="nav-link" href="{{ route('register') }}">新規登録</a>
+                  @endif
+                </li>
+              @else
+                <li class="nav-item">
+                  <a class="nav-link" href="{{ url("/list") }}">ちくわ一覧</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="{{ url("/mypage") }}">マイページ</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="{{ url("/cart") }}">カート</a>
+                </li>
+                <li>
+                  <a class="nav-link" href="{{ route('logout') }}"
+                   onclick="event.preventDefault();
+                            document.getElementById('logout-form').submit();">
+                    ログアウト
+                  </a>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">ログイン</a>
-                            </li>
-                            <li class="nav-item">
-                                @if (Route::has('register'))
-                                    <a class="nav-link" href="{{ route('register') }}">新規登録</a>
-                                @endif
-                            </li>
-                        @else
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ url("/list") }}">ちくわ一覧</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ url("/mypage") }}">マイページ</a>
-                            </li>
-                            <li>
-                                <a class="nav-link" href="{{ route('logout') }}"
-                                   onclick="event.preventDefault();
-                                                 document.getElementById('logout-form').submit();">
-                                    ログアウト
-                                </a>
+                  <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    @csrf
+                  </form>
+                </li>
+              @endguest
+            </ul>
+          </div>
+        </div>
+      </nav>
 
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                    @csrf
-                                </form>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        <main class="py-4">
-            @yield('content')
-        </main>
+      <main class="py-4">
+          @yield('content')
+      </main>
     </div>
-</body>
+  </body>
 </html>
