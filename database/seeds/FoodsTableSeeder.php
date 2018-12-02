a916d46f407d685d2f5f1577095ea9d651159d13
takashi <takashioicsn@gmail.com>
2018-12-02 23:03:59 +0900

    seeder修正 レイアウト修正

--- database/seeds/FoodsTableSeeder.php
+++ database/seeds/FoodsTableSeeder.php
@@ -15,29 +15,28 @@ class FoodsTableSeeder extends Seeder
       Food::truncate();
 
       $name = [
-        'とうふちくわ', 'ネギ入りとうふちくわ', '野焼きちくわ', '竹付きちくわ', '白ちくわ', 'あごちくわ'
+        '豆腐ちくわ', 'ネギ入り豆腐ちくわ', '竹付きちくわ', '白ちくわ', 'あごちくわ'
       ];
 
       $description = [
-        'とうふちくわ...',
-        'ネギ入りとうふちくわ...',
-        '野焼きちくわは...',
-        '竹付きちくわは...',
-        '白ちくわは...',
-        'あごちくわは...'
+        '江戸時代から続く鳥取の伝統の味です。鳥取県産大豆を使用した木綿豆腐を７０％、白身魚を３０％使用しています。',
+        'とうふちくわにネギを混ぜ込んだちくわです。とうふちくわとネギとの相性は絶品で、一口食べると口の中いっぱいにネギの風味が広がります。',
+        '天然の竹を用い、こんがりと色良く焼きあげいます。平安時代末期から食べられており、竹付きのままかじりつく食べ方が最高です。',
+        'スケトウダラを原料としたちくわです。焼き色を付けずに仕上げ、皮がやわらかく、肉厚です。',
+        '新鮮なとびうおを原料としたちくわです。わさび醤油をつけて食べるとまた違った味が楽しめます。'
       ];
 
       $price = [
-        '180', '200', '150', '150', '180', '700'
+        '180', '200', '150', '180', '700'
       ];
 
       $img = [
         'http://placehold.it/300/?text=fish%20paste1', 'http://placehold.it/300/?text=fish%20paste2',
         'http://placehold.it/300/?text=fish%20paste3', 'http://placehold.it/300/?text=fish%20paste4',
-        'http://placehold.it/300/?text=fish%20paste5', 'http://placehold.it/300/?text=fish%20paste6'
+        'http://placehold.it/300/?text=fish%20paste5'
       ];
 
-      for($i = 0; $i < 6; $i++) {
+      for($i = 0; $i < 5; $i++) {
         DB::table('foods')->insert([
           'name' => $name[$i],
           'description' => $description[$i],
--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -5,7 +5,7 @@
     <h3 class="mb-5">カート</h3>
     @if($foods)
       @foreach($foods as $food)
-        <div class="row border-bottom">
+        <div class="row border-bottom mx-0">
           <div class="col align-self-center">{{ $food->name }}</div>
           <div class="col-auto text-right align-self-center">{{ $amount[$count] }}つ</div>
           <div class="col-auto text-right align-self-center">{{ $price[$count++] }}円</div>
