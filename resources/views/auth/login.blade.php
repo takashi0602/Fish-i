49595065ced8c8a50f05f4a715ba52d6c0545472
takashi <takashioicsn@gmail.com>
2018-12-10 10:28:59 +0900

    ログインページ 修正

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,44 +2,44 @@
 
 @section('content')
 <div class="container">
-<div class="mb-4">ログイン</div>
-<form method="POST" action="{{ route('login') }}">
-    @csrf
-    <div class="form-group row">
-        <label for="email" class="col-sm-3 col-form-label">メールアドレス</label>
-        <div class="col-md-9">
-            <input id="email" type="text" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}">
-            @if(!empty($errors->first('email')))
-                <span class="text-danger">※{{ $errors->first('email') }}</span>
-            @endif
-        </div>
-    </div>
-    <div class="form-group row">
-        <label for="password" class="col-md-3 col-form-label">パスワード</label>
-        <div class="col-md-9">
-            <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password">
-            @if(!empty($errors->first('password')))
-                <span class="text-danger">※{{ $errors->first('password') }}</span>
-            @endif
-        </div>
-    </div>
-    <div class="form-group row">
-        <div class="col-md-6 offset-md-3">
-            <div class="form-check">
-                <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-                <label class="form-check-label" for="remember">
-                    パスワードを記憶する
-                </label>
+    <h3 class="mb-4">ログイン</h3>
+    <form method="POST" action="{{ route('login') }}">
+        @csrf
+        <div class="form-group row">
+            <label for="email" class="col-sm-3 col-form-label">メールアドレス</label>
+            <div class="col-md-9">
+                <input id="email" type="text" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}">
+                @if(!empty($errors->first('email')))
+                    <span class="text-danger">※{{ $errors->first('email') }}</span>
+                @endif
             </div>
         </div>
-    </div>
-    <div class="form-group row mb-0">
-        <div class="col-md text-right">
-            <button type="submit" class="btn btn-primary">
-                ログイン
-            </button>
+        <div class="form-group row">
+            <label for="password" class="col-md-3 col-form-label">パスワード</label>
+            <div class="col-md-9">
+                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password">
+                @if(!empty($errors->first('password')))
+                    <span class="text-danger">※{{ $errors->first('password') }}</span>
+                @endif
+            </div>
         </div>
-    </div>
-</form>
+        <div class="form-group row">
+            <div class="col-md-6 offset-md-3">
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                    <label class="form-check-label" for="remember">
+                        パスワードを記憶する
+                    </label>
+                </div>
+            </div>
+        </div>
+        <div class="form-group row mb-0">
+            <div class="col-md text-right">
+                <button type="submit" class="btn btn-primary">
+                    ログイン
+                </button>
+            </div>
+        </div>
+    </form>
 </div>
 @endsection
